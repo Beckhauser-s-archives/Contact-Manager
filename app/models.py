250930338837712b824b47181f4dd27fe9b24234
Beckhauser <beckhauser125@gmail.com>
2019-10-30 09:01:08 -0300

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils.timezone import datetime
+
+# Create your models here.
+# Quando fazer uma alteração 
+# python manage.py makemigrations
+# python manage.py migrate
+class Contact(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    phone = models.IntegerField()
+    info = models.CharField(max_length=30)
+    gender = models.CharField(max_length=50, choices =(
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ))   
+    image = models.ImageField(upload_to='images/', blank = True)
+    #date_added = models.DateField(auto_now_add=True) 
+    date_added = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
