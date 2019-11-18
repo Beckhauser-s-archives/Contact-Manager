68152d3b2c78d2aaf17d476334792318625752ef
Lucas <beckhauser125@gmail.com>
2019-11-17 22:47:38 -0300

    Change the project's name

--- contactsmanager/wsgi.py
+++ contactsmanager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for banana project.
+WSGI config for contactsmanager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banana.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactsmanager.settings')
 
 application = get_wsgi_application()
